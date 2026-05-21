7f3141b212886c4d14bc9effc0f2bb57d64d1ad5
gleidsonfagno <gleidsonfagner838@gmail.com>
2026-05-21 20:06:13 -0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,11 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            System.out.println(input + ": command not found");
-
             if (input.equals("exit")) {
                 break;
             }
+            System.out.println(input + ": command not found");
+
         }
         while (true);
     }
